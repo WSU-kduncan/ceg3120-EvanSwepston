cb1a7a56f9ba954cf022770802b5a3f2b7a3069b
Ubuntu <swepston.2@wright.edu>
2022-01-28 22:30:29 +0000

    Added a functioning discord bot

---
+++ Projects/Project1/Discord-Bot/README.md
@@ -0,0 +1,10 @@
+# Setup
+- After going to discord's developer portal and creating a new application, you can then create a bot. Below the bot's username is an API token that can be copied and used, or regenerated if necessary.
+- The token must be put in a file named `.env`
+- To run, you need to install python3, pip, discord.py, and python-dotenv
+
+# Usage
+- If you type the command `Chidi` into chat, the bot will respond with a random quote from The Good Place
+
+# Research
+- To keep the discord bot working 24/7, you could set it up on a raspberry pi that is constantly plugged in and running.As long as your power doesn't go out the bot will be able to work.
---
+++ Projects/Project1/Discord-Bot/bot.py
@@ -0,0 +1,51 @@
+import os
+
+import discord
+import random
+from dotenv import load_dotenv
+
+load_dotenv()
+#print(os.getenv('DISCORD_TOKEN'))
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    hitchhiker_quotes = [
+            'There is an art, it says, or rather, a knack to flying. The knack lies in learning how to throw yourself at the ground and miss.',
+            'It is a mistake to think you can solve any major problems just with potatoes.',
+            'In the beginning the Universe was created. This has made a lot of people very angry and been widely regarded as a bad move.',
+            'A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.',
+    ]
+
+    good_place_quotes = [
+            'The real Bad Place was the friends we made along the way.',
+            'Why do bad things always happen to mediocre people who are lying about their identities?',
+            'Man, repressing your feelings is great',
+    ]
+
+    if message.content == 'Chidi':
+        #response = random.choice(brooklyn_99_quotes)
+        #response = random.choice(hitchhiker_quotes)
+        response = random.choice(good_place_quotes)
+        await message.channel.send(response)
+
+client.run(TOKEN)
